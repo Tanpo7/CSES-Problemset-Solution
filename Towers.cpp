3b3ef4b3cede158de642634af1ad665c3cf47157
Tanpo7 <95082877+Tanpo7@users.noreply.github.com>
2023-06-13 19:56:38 +0530

    Add files via upload

---
+++ Towers.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define endl "\n"
+#define take(x)       \
+    for (auto &y : x) \
+        cin >> y;
+#define show(x)           \
+    for (auto y : x)      \
+        cout << y << " "; \
+    cout << endl;
+#define pb push_back
+#define mp make_pair
+#define max3(a, b, c) max(max((a), (b)), (c))
+#define max4(a, b, c, d) max(max((a), (b)), max((c), (d)))
+#define min3(a, b, c) min(min((a), (b)), (c))
+#define min4(a, b, c, d) min(min((a), (b)), min((c), (d)))
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) v.begin(), v.end()
+ll M = 998244353;
+#define MAX 100000
+ll mod = 1e9 + 7;
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    multiset<int> m;
+    for(int i = 0;i<n;i++)
+    {
+        int k;
+        cin>>k;
+        auto it = m.upper_bound(k);
+        if(it == m.end())
+        {
+            m.insert(k);
+        }
+        else
+        {
+            m.erase(it);
+            m.insert(k);
+        }
+    }
+    cout<<m.size()<<endl;
+    return;
+}
+
+int main()
+{
+    int tt;
+    tt = 1;
+    while (tt--)
+    {
+        solve();
+    }
+
+    return 0;
+}
